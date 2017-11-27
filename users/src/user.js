b3db350d629ab99cffe22633d5f48b5fe74bc138
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-11-27 09:21:36 -0900

    [Section 6, Lecture 18] - Setup User Schema & exported Module

--- users/src/user.js
+++ users/src/user.js
@@ -1,2 +1,10 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  name: String
+});
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
