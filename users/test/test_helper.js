04a4c9c234404bb1aa3472105c583fc96f759031
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-11-28 10:35:31 -0900

    [Section 6, Lecture 26] - Implemented `before()`, so that the connection to the Database is only opened once during the run of out test suite.

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -4,16 +4,20 @@ const mongoose = require('mongoose');
 /* The default promise  provided with Mongoose is Deprecated, they advised you to use your own promise library. Set Mongoose's default promise library to ES6 Promise withing Nodejs  */
 mongoose.Promise = global.Promise;
 
-/* Note that the `connect` below, the database 'user_test' doesn't need to exsit right off the bat. Mongoose works with MongoDb, when you try to save data to database, it will create it at that time.*/
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
 
-  /*  'open' & 'error' are specific strings that we need to use with Mongoose for their respecitive callsbacks */
-  .once('open', () => {})
-  .on('error', (error) => {
-    console.warn('Warning', error);
-  });
+/* `before()` is executed ONCE before all of the test suite in Mocha. Implemented this, so that the connection is opened ONCE during the execution of the test suite */
+before((done) => {
 
+  /* Note that the `connect` below, the database 'user_test' doesn't need to exsit right off the bat. Mongoose works with MongoDb, when you try to save data to database, it will create it at that time.*/
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection
+
+    /*  'open' & 'error' are specific strings that we need to use with Mongoose for their respecitive callsbacks */
+    .once('open', () => done())
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
 
   beforeEach((done) => {
     mongoose.connection.collections.users.drop(() => {
