1d917d4ffed8d5659cb9877d67cfa0f0bf08bf8a
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-11-28 10:30:38 -0900

    [Section 6 Lecture 26] - Corrected Depraction Warning with Mongoose's Default Promise Library. Set it do ES6 Promises.

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
+
+/* The default promise  provided with Mongoose is Deprecated, they advised you to use your own promise library. Set Mongoose's default promise library to ES6 Promise withing Nodejs  */
+mongoose.Promise = global.Promise;
+
 /* Note that the `connect` below, the database 'user_test' doesn't need to exsit right off the bat. Mongoose works with MongoDb, when you try to save data to database, it will create it at that time.*/
 mongoose.connect('mongodb://localhost/users_test');
 mongoose.connection
 
   /*  'open' & 'error' are specific strings that we need to use with Mongoose for their respecitive callsbacks */
-  .once('open', () => console.log('Good to go!'))
+  .once('open', () => {})
   .on('error', (error) => {
     console.warn('Warning', error);
   });
