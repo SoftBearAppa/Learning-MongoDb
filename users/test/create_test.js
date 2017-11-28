77eedcf9f16515f0df4b9e78d970772f2e874e39
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-11-28 10:20:38 -0900

    [Section 6, Lecture 25] - Added `assert()` to test whether `joe` has been saved or not to the Database.

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -2,9 +2,17 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating records', () => {
-  it('saves a user', () => {
+
+  // `done()` is avaiable to each `it()` block as well.
+  it('saves a user', (done) => {
     const joe = new User({ name: 'Joe' });
 
-    joe.save();
+    joe.save()
+      .then(() => {
+
+        /* `.isNew` is a property avaiable from Mongoose on all Model instances that is created. the `.isNew` is asssigned to be `true` before it is saved to the database. Once is it saved, `.isNew` becomes evaluated to `false`. We assert that the value for `joe.isNew` is now false, to test that is has been saved to the database. */
+        assert(!joe.isNew);
+        done();
+      });
   });
 });
