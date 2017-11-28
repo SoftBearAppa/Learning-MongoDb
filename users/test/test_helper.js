d7bf4bff89c9bc2ae3f869d7e33a6abf3a4e970c
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-11-28 09:38:35 -0900

    Corrected typo in comments

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
- /* Note that the `connect` below, the database 'user_test' doesn't need to exsit right off the bat. Mongoose works with MongoDb, when yuo try to save data to database, it will create it at that time.*/
+ /* Note that the `connect` below, the database 'user_test' doesn't need to exsit right off the bat. Mongoose works with MongoDb, when you try to save data to database, it will create it at that time.*/
 mongoose.connect('mongodb://localhost/users_test');
 mongoose.connection
   .once('open', () => console.log('Good to go!'))
