f974f9e691b42240756d81b3f1fe1e2b4fbba363
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-11-29 14:04:43 -0900

    [Section 6, Lecture 37] - Refactored `assert()` statements in `it()` block. Created another test using the `.update()` method on a Model Instance

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -12,14 +12,12 @@ describe('Updating records', () => {
       .then(() => done());
   });
 
-  /* Test updating data in the Database using `.set()` method. This method only updates an Instance. */
-  it('Model Instance using set & save', (done) => {
-
-    joe.set('name', 'Alex');
-
-    /* Notice that after we call the `.set()` method, that we do have to `.save()` the new change to the database. */
-    joe.save()
+  /* `assertName()` takes in a method that is being passed into it and then assets whether the method completed the change. It is also passed 'done' from the `it()` block so it can sign off that the test is complete.*/
+  function assertName(operation, done) {
+    operation
       .then(() => {
+
+        /* Not passing any data into the `.find()` method, will pull all model instances with the User Class */
         User.find({})
           .then((users) => {
             assert(users.length === 1);
@@ -27,5 +25,20 @@ describe('Updating records', () => {
             done();
           });
       });
+  }
+
+  /* Test updating data in the Database using `.set()` method. This method only updates an Instance. */
+  it('Model Instance using set & save', (done) => {
+
+    // Notice that we are looking for a property called 'name' with the value that we want to update it to.
+    joe.set('name', 'Alex');
+
+    /* Notice that after we call the `.set()` method, that we do have to `.save()` the new change to the database. */
+    assertName(joe.save(), done);
+  });
+
+  /* Test updating data using the `.update()` method. This method can update bulk properties/values on a Model Instance. */
+  it('A Model Instancecan update', (done) => {
+    assertName(joe.update({ name: 'Alex' }), done);
   });
 });
