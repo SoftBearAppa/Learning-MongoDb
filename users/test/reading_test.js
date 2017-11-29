21900ba70c4b759f36a97e0ef0b0dc6444fc3643
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-11-29 09:30:39 -0900

    [Section 6, Lecture 31] - Added another `it()` to test reading data from database using the `.findOne()` method. Also updated commenets.

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -17,9 +17,19 @@ describe('Reading users out of the database', () => {
     User.find({ name: 'Joe' })
       .then((user) => {
 
-        /* The returned value for user & joe is an object that as a string {ObjectId( *then some string*)}; We need to Stringify the value to read it correctly. Aso when the Instance that `joe()` is created, Mongoose assigns it an ID before it gets saved to the Database as well. */
+        /* The returned value for user & joe is an object that as a string {ObjectId( *then some string*)}; We need to Stringify the value to read it correctly, so that JS knows how to make the comparison. Aso when the Instance that `joe()` is created, Mongoose assigns it an ID before it gets saved to the Database as well. */
         assert(user[0]._id.toString() === joe._id.toString())
         done();
       })
+  });
+
+  it('find a user with aparticular id', (done) => {
+
+    /* `.findOne` finds the first instance in the Database. It expects an object, not a string. We don't need to Stringify the id, because Mongoose knows how to work/deal with the ID Object. */
+    User.findOne({ _id: joe._id })
+      .then((user) => {
+        assert(user.name === 'Joe');
+        done();
+      });
   });
 });
