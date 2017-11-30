7aeeddee637025e700651b1d28f9d309999e183b
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-11-30 10:50:16 -0900

    [Section 7, Lecture 42] - Setup validation to test whether a `User()` instance that has the property value of 'name'  as undefined & to insure that validation is working correctly.

--- users/test/validation_test.js
+++ users/test/validation_test.js
@@ -3,7 +3,19 @@ const User = require('../src/user');
 
 describe('Validation records', () => {
 
-  it('Requires a user name', () => {
+  /* Tests whether the validation is working */
+  it('Requires a user name', (done) => {
 
+    /*  Create a new `User()` instance. Explicated putting in 'undefined' as value, so that it is clear that there should be a valid value. */
+    const user = new User({ name: undefined });
+
+    /* This gives an asynchronous result on whether the instance of `user` was valid. */
+    const validationResult = user.validateSync();
+
+    /* Drills down to where the error message is located, back in the `User()` Schema */
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name is required.')
+    done();
   });
 });
