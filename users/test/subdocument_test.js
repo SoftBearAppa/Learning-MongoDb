5c345ea6b2d65260d50733bbddc112e71322083c
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-12-03 10:18:45 -0900

    [Section 8, Lecture 48] - Created another validation to test if we could save a subdocument to an exsisting record in the database.

--- users/test/subdocument_test.js
+++ users/test/subdocument_test.js
@@ -17,4 +17,22 @@ describe('Subdocuments', () => {
         done();
       })
   });
+
+  /* Test whether we can add a subdocument to an exsisting record in the database. */
+  it('add subdocument to exsisting record', (done) => {
+    const joe = new User({ name: 'Joe' });
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+
+        // `Array.push()` is a method sraight from vanilla JS
+        user.posts.push({ title: 'Added Subdocument' });
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe'}))
+      .then((user) => {
+        assert(user.posts[0].title === 'Added Subdocument');
+        done();
+      });
+  });
 });
