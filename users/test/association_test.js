8b99d97c63278330e0ed5b07e71577f7cf4b463a
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-12-06 14:36:51 -0900

    [Section9, Lecture 62] - Finished creating last validation. Explored using the `.populate()` 'modifier' when creating a query.

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -33,11 +33,43 @@ describe('Associations',() => {
   it('saves a relation between a user & a blogpost', (done) => {
     User.findOne({ name: 'Joe' })
 
-      /* You can add 'modifiers' to the query like: `.populate()`. This tells Mongoose to recrusivly load the 'blogPost' data, assocaited with the user `joe()` */
+      /* You can add 'modifiers' to the query like: `.populate()`. This tells Mongoose to recursivley load the 'blogPost' data, assocaited with the user `joe()` */
       .populate('blogPost')
       .then((user) => {
         assert(user.blogPost[0].title === 'JS is Great!');
         done();
       })
+  });
+
+  it('saves a full relation graph', (done) => {
+    User.findOne({ name: 'Joe' })
+
+    /* We can also pass `.populate()` an object to recursivley load more data. Note that long querys like below can cause longer delays or even crash the database due it taking a long time to recursivly load all the data being requested; This is a bigger issue when working with a MUCH larger database */
+    .populate({
+
+      // The field name under `User()` that we are trying to load
+      path: 'blogPost',
+
+      // Think of this like chaining `.populate()`
+      populate: {
+
+        //The field name unser `BlogPost()` that we are trying to load
+        path: 'comments',
+
+        //Mongoose Requires that we tell it what model to look under with the 'model' field.
+        model: 'comment',
+        populate: {
+          path: 'user',
+          model: 'user'
+        }
+      }
+    })
+    .then((user) => {
+      assert(user.name === 'Joe');
+      assert(user.blogPost[0].title === 'JS is Great!');
+      assert(user.blogPost[0].comments[0].content === 'Congratz on a great post!');
+      assert(user.blogPost[0].comments[0].user.name === 'Joe');
+      done();
+    })
   });
 })
