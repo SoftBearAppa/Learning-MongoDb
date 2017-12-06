ab0018c8f59ad5c5547c5c97c177cd8bbe950488
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-12-06 11:14:38 -0900

    [Section 9, Lecture 57] - Setup Model for `BlogPost()`

--- users/src/blogPost.js
+++ users/src/blogPost.js
@@ -1,2 +1,19 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
+const blogPostSchema = new Schema({
+  title: String,
+  content: String,
+  comments: [{ 
+
+    // Type of reffernce to point of in a different collection
+    type: Schema.Types.ObjectId,
+
+    // Refferencing the 'Comment' Collection; Working with Mongoose. It tells Mongoose to look in the 'Comment' Collection
+    ref: 'Comment'
+  }]
+});
+
+const BlogPost = mongoose.model('blogPost', blogPostSchema);
+
+module.exports = BlogPost;
