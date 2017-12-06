d94b3e7f71d4d2dc26c45a488491745d1b258ac1
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-12-06 11:33:00 -0900

    [Section 9, Lecture 58] - Setup 'association_test.js', added boilerplate.

---
+++ users/test/association_test.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const User = require('../src/user');
+const BlogPost = require('../src/blogPost');
+const Comment = require('../src/comment');
+const assert = require('assert');
+
+describe('Associations',() => {
+  
+})
