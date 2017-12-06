80b778456f34327a66f203eb62d0972eee8c6e93
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-12-06 14:00:52 -0900

    [SEction 9, Lecture 60] - Setup basic validation to see a relation between a 'user' & a 'blogPost'.

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -21,5 +21,20 @@ describe('Associations',() => {
 
     //This is the same as above, Mongoose will automatically just refference the ObjectID.
     comment.user = joe;
+
+    // `Promise.all()` is an ES6 feature. Also that we need to save the data to the Database. Before we save, all the data is only in Mongoose Memory.
+    Promise.all([joe.save(), blogPost.save(), comment.save()])
+      .then(() => {
+        done();
+      });
+  });
+
+  /* `it.only()` Tells Mocha to ignore all the other validation test, & just run this */
+  it.only('saves a relation between a user & a blogpost', (done) => {
+    User.findOne({ name: 'Joe' })
+      .then((user) => {
+        console.log(user);
+        done();
+      })
   });
 })
