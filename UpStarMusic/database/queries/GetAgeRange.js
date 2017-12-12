43169656cea908a79744594f659345867b0bfb45
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-12-12 12:15:14 -0900

    [Section 13, Lecture 84] - Setup query to get Min & max values of 'age' for all Artists in the Database.

--- UpStarMusic/database/queries/GetAgeRange.js
+++ UpStarMusic/database/queries/GetAgeRange.js
@@ -6,4 +6,18 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  const min = Artist.find({})
+    .sort({ age: 1 })
+    .limit(1)
+    .then((artists) => artists[0].age);
+
+  const max = Artist.find({})
+    .sort({ age: -1 })
+    .limit(1)
+    .then((artists) => artists[0].age);
+
+  return Promise.all([ min, max ])
+    .then((r) => { 
+      return { min: r[0], max: r[1] }
+    });
 };
