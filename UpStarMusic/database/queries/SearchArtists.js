610420d54017f2a46470e5098fe2eb98ba31db26
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-12-12 17:01:24 -0900

    [Section 13, Lecture 86] - Setup 'SearchArtist.js'. Setup basic function exluding 'criteria'.

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -9,4 +9,13 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  const query = Artist.find({})
+    .sort({ [sortProperty]: 1})
+    .skip(offset)
+    .limit(limit)
+
+  return Promise.all([ query, Artist.count()])
+    .then((r) => {
+      return { all: r[0], count: r[1], offset, limit }
+    })
 };
