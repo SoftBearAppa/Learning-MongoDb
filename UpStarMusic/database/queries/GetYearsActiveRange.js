502646e51feb8ab9b77a40bc425977a130351573
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-12-12 16:09:24 -0900

    [Section 13, Lecture 84] - Setup Query to sort, & to find min & max values of Years Active.

--- UpStarMusic/database/queries/GetYearsActiveRange.js
+++ UpStarMusic/database/queries/GetYearsActiveRange.js
@@ -6,4 +6,18 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  const minYears = Artist.find({})
+    .sort({ yearsActive: 1 })
+    .limit(1)
+    .then((artists) => artists[0].yearsActive);
+
+  const maxYears = Artist.find({})
+    .sort({ yearsActive: -1 })
+    .limit(1)
+    .then((artists) => artists[0].yearsActive);
+
+  return Promise.all([minYears, maxYears])
+    .then((r) => {
+      return { min: r[0], max: r[1] }
+    });
 };
