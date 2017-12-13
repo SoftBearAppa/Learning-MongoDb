e4ae193b522b6e9d7ef1a75b31e8b212ee5eab83
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-12-12 21:49:55 -0900

    [Section 13, Lecture 88] - Started to setup function to filter through query criteria, to be passed into `Artist.find()`

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -9,7 +9,7 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
-  const query = Artist.find({})
+  const query = Artist.find(buildQuery(criteria))
     .sort({ [sortProperty]: 1})
     .skip(offset)
     .limit(limit)
@@ -19,3 +19,24 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
       return { all: r[0], count: r[1], offset, limit }
     })
 };
+
+const buildQuery = (criteria) => {
+  const query = {};
+  const { age, yearsActive, name } = criteria;
+
+  if(age) {
+    query.age = {
+      $gte: age.min,
+      $lte: age.max
+    };
+  };
+
+  if(yearsActive) {
+    query.yearsActive = {
+      $gte: yearsActive.min,
+      $lte: yearsActive.max
+    };
+  };
+
+  return query;
+}
