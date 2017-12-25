a2021cd9f05825ed376c786bd25fe2438da13740
SoftBearFeet <SoftBearFeet@users.noreply.github.com>
2017-12-24 16:02:58 -0900

    Setup error handling on '/api/driver''

--- Muber/app.js
+++ Muber/app.js
@@ -15,4 +15,9 @@ if (process.env.NODE_ENV !== 'test') {
 app.use(bodyParser.json());
 routes(app);
 
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err.message });
+  
+});
+
 module.exports = app;
--- Muber/controllers/drivers_controller.js
+++ Muber/controllers/drivers_controller.js
@@ -5,9 +5,10 @@ module.exports = {
     res.send({hi: 'there'});
   },
 
-  create(req, res) {
+  create(req, res, next) {
     const driverProps = req.body;
     Driver.create(driverProps)
-      .then(driver => res.send(driver));
+      .then(driver => res.send(driver))
+      .catch(next);
   },
 }
